787aa39417da123062245accdac3193cc6bea359
RoyRaudales368 <126222770+RoyRaudales368@users.noreply.github.com>
2024-03-16 21:59:29 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('mostrarFormulario').addEventListener('click', function() {
+    document.getElementById('formularioContacto').style.display = 'block';
+});
+
+document.getElementById('cerrarFormulario').addEventListener('click', function() {
+    document.getElementById('formularioContacto').style.display = 'none';
+});
+
